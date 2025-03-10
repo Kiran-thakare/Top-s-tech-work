626c9f249be9c3d7dac360e18080db1df21d9e5b
Kiran-thakare <kiranthakare227@gmail.com>
2025-03-10 16:33:11 +0530

    add

--- Project/A00_Ecommerce_Project/src/main/java/com/repo/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.model.Product;
-
-public interface ProductRepo extends JpaRepository<Product, Integer> {
-
-}
--- Project/A00_Ecommerce_Project/src/main/java/com/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.model.User;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-
-}
