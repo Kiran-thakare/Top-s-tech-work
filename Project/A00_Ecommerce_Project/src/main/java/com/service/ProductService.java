ca58f6fb400e3bfea4bcbe992f7572c9b5189b79
Kiran-thakare <kiranthakare227@gmail.com>
2025-03-10 16:33:54 +0530

    add

---
+++ Project/A00_Ecommerce_Project/src/main/java/com/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.service;
+
+import java.util.List;
+
+import com.model.Product;
+
+public interface ProductService {
+   
+	public void addOrUpdateProduct(Product product);
+	
+	public List<Product> allProducts();
+	
+	public Product productById(int id);
+	
+	public void deleteById(int id);
+	
+}
---
+++ Project/A00_Ecommerce_Project/src/main/java/com/service/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package com.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.model.User;
+import com.repo.UserRepo;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserRepo repo;
+
+	@Override
+	public void addOrUpdateUser(User user) {
+		// TODO Auto-generated method stub
+		repo.save(user);
+
+	}
+
+	@Override
+	public List<User> allUsers() {
+		// TODO Auto-generated method stub
+		return repo.findAll();
+	}
+
+	@Override
+	public User getUserById(int id) {
+		// TODO Auto-generated method stub
+		return repo.findById(id).orElseThrow();
+	}
+
+	@Override
+	public void deleteUserById(int id) {
+		// TODO Auto-generated method stub
+		repo.deleteById(id);
+
+	}
+
+}
